ab92e1ad8bd7c6dbf86a58adfe4c51311810a0a7
Shauryaditya <shauryadityab@gmail.com>
2025-02-02 21:31:36 +0530

    feat: integrate @shauryadity/medium-common for blog and user input validation

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { createBlogInput, updateBlogInput } from "@shauryadity/medium-common";
 import { Hono } from "hono";
 import { verify } from "hono/jwt";
 
@@ -51,6 +52,11 @@ blogRouter.post('/', async(c) => {
     console.log("Author ID: ", userId);
     
     const body = await c.req.json();
+    const { success } = createBlogInput.safeParse(body);
+    if (!success) {
+        c.status(411);
+        return c.json({ message: "Invalid input" });
+    }
     const prisma = new PrismaClient({
         datasourceUrl: c.env?.DATABASE_URL
     }).$extends(withAccelerate());
@@ -69,6 +75,11 @@ blogRouter.post('/', async(c) => {
 
 blogRouter.put('/', async(c) => {
     const body = await c.req.json();
+    const { success } = updateBlogInput.safeParse(body);
+    if (!success) {
+        c.status(411);
+        return c.json({ message: "Invalid input" });
+    }
    const prisma = new PrismaClient({
        datasourceUrl: c.env?.DATABASE_URL
    }).$extends(withAccelerate());
