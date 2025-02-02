77fd787720aad4901c21228444292fc4219d442b
Shauryaditya <shauryadityab@gmail.com>
2025-02-02 12:13:07 +0530

    feat: add Zod validation for user signup input and update package name

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { sign } from 'hono/jwt';
+import { signupInput } from '../../../common/src/index';
 
 
 
@@ -22,7 +23,11 @@ userRouter.post('/signup', async (c) => {
 		datasourceUrl: c.env?.DATABASE_URL,
 	}).$extends(withAccelerate());
 	const body = await c.req.json();
-
+	const {success} = signupInput.safeParse(body);
+	if (!success) {
+		c.status(411);
+		return c.json({message: "Invalid input"})
+	}
 	try {
 		const user = await prisma.user.create({
 			data: {
